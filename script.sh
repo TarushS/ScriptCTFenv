e60105602bc784378d207c87fcf36e2eef05b671
TarushS <tsonakya@gmail.com>
2021-05-22 23:09:13 +0530

    Updated script.sh

--- script.sh
+++ script.sh
@@ -1,62 +1,48 @@
 #!/bin/bash
-cd /opt/
-git clone https://github.com/TarushS/ctfenv
-cd ctfenv
-mv picossh.sh ..
-mv finish.sh ..
-cd ..
-rm -r ctfenv
-cd ~
-rm .bashrc
-git clone https://github.com/TarushS/bashrc
-cd bashrc
-mv .bashrc ..
-cd ..
-rm -r bashrc
-mkdir tools
-cd tools
-git clone https://github.com/niklasb/libc-database
-cd libc-database
-chmod +x get
-./get
-cd ..
-git clone https://github.com/JonathanSalwan/ROPgadget
-apt install python -y
-apt install python-pip -y
-pip install pwntools
-apt install python3 -y
-apt install python3-pip -y
+cd
+
+# Installing dependencies
+sudo apt update -y
+sudo apt-get install python python-pip -y
+sudo apt-get install python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential ruby -y
+pip3 install pwntools
 pip install capstone
-cd ROPgadget
+
+# Installing Tools
+mkdir tools
+
+## ROPGadget
+git clone https://github.com/JonathanSalwan/ROPgadget && cd ROPgadget
 python setup.py install
 cd ..
-git clone https://github.com/xct/ropstar
-cd ..
-mkdir CTF
-cd CTF
-cd ..
-cd tools
-apt-get update
-apt-get install python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential -y
-pip3 install pwntools
-apt install exiftool -y
-apt install ghex -y
-apt install binwalk -y
-apt install steghide -y
-mkdir stegsolve
-cd stegsolve
+
+## Forensic tools
+sudo apt-get install exiftool eog ghex binwalk steghide stegsnow pngcheck zbar-tools fcrackzip wine -y
+gem install zsteg
+
+## Stegsolve
+mkdir stegsolve && cd stegsolve
 wget http://www.caesum.com/handbook/Stegsolve.jar -O stegsolve.jar
-apt-get install zbar-tools -y
-apt install stegsnow -y
-apt install pngcheck -y
-apt install fcrackzip -y
-cd ..
+cd..
+
+## Jsteg
 wget -O /usr/bin/jsteg https://github.com/lukechampine/jsteg/releases/download/v0.1.0/jsteg-linux-amd64
 chmod +x /usr/bin/jsteg
 wget -O /usr/bin/slink https://github.com/lukechampine/jsteg/releases/download/v0.2.0/slink-linux-amd64
 chmod +x /usr/bin/slink
-apt install ruby -y
-gem install zsteg
-apt install wine -y
-cd
-source .bashrc
+
+## GDB peda+gef+pwndbg
+git clone https://github.com/soaringk/gdb-peda-pwndbg-gef.git
+cd gdb-peda-pwndbg-gef
+./install.sh
+cd ..
+
+## Misc
+if uname -r |grep -qi 'microsoft' ; then
+  echo -e "\nexport DISPLAY=$(awk '/nameserver / {print $2; exit}' /etc/resolv.conf 2>/dev/null):0" >> ~/.bashrc
+  echo "export LIBGL_ALWAYS_INDIRECT=1" >> ~/.bashrc
+  echo "export PATH=\"$PATH:/mnt/c/Windows/system32:/mnt/c/Windows\"" >> ~/.bashrc
+  echo -e "alias explorer=\"explorer.exe\"\nalias nautilus=\"explorer.exe .\"" >> ~/.bash_aliases
+fi
+
+sudo apt install neofetch -y
